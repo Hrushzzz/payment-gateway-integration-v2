134fcb17dbeabe60cb0ef6385f7cb4c3ddcac6d2
SpiderMan <hrushikesh.tk@gmail.com>
2026-03-01 02:23:50 +0530

    Payment Service initial setup

---
+++ src/main/java/com/hrushikesh/payment_gateway_integration_v2/PaymentGatewayIntegrationV2Application.java
@@ -0,0 +1,13 @@
+package com.hrushikesh.payment_gateway_integration_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGatewayIntegrationV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentGatewayIntegrationV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrushikesh/payment_gateway_integration_v2/PaymentGatewayIntegrationV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrushikesh.payment_gateway_integration_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayIntegrationV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
